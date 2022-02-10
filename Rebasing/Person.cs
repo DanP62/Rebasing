070d444532e82bb9a2a0e3c497ba3adb25c28e83
DanP62 <dan.pirvanescu@yahoo.com>
2022-02-10 13:47:43 +0000

    Fist commit

---
+++ Rebasing/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rebasing
+{
+    public class Person
+    {
+        public double Age { get; set; }
+
+
+    }
+}
